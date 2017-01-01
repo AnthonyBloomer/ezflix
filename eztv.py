8482c56f8faf4b8ad599dd14072e9f29211fd7a2
Anthony Bloomer <ant0@protonmail.ch>
2017-01-01 21:23:20 +0000

    Create eztv.py

---
+++ eztv.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import requests
+import subprocess
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('query')
+
+args = parser.parse_args()
+
+query = args.query.replace(' ', '-').lower()
+url = 'https://eztv.ag/search/' + query
+
+req = requests.get(url)
+soup = BeautifulSoup(req.text, 'html.parser')
+
+results = []
+i = 1
+if __name__ == '__main__':
+    print 'Searching...'
+
+    for magnets in soup.find_all('a', {'class': 'magnet'}, href=True):
+        if magnets is None:
+            sys.exit('No results found')
+        results.append({'id': i, 'title': magnets['title'], 'magnet': magnets['href']})
+        i += 1
+
+    print 'Results...'
+
+    for result in results:
+        print '%s %s' % (result['id'], result['title'])
+
+    print 'Select show'
+
+    while True:
+        read = input()
+        for result in results:
+            if result['id'] == read:
+                print 'Playing %s!' % result['title']
+                command = 'peerflix "%s" --mpv' % result['magnet']
+                subprocess.Popen(['/bin/bash', '-c', command])
