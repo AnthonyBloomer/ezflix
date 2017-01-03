6d701e82da45fde127e56106bf1be34a91d43190
Anthony Bloomer <ant0@protonmail.ch>
2017-01-03 04:32:24 +0000

    Update ezflix.py

--- ezflix.py
+++ ezflix.py
@@ -30,14 +30,15 @@ def show(q):
 
 
 def movie(q):
-    req = requests.get('https://yts.ag/api/v2/list_movies.json?query_term=%s' % q)
+    req = requests.get('https://yts.ag/api/v2/list_movies.json?query_term=%s&sort_by=seeds&limit=50' % q)
     if req.status_code == 200:
         req = req.json()
         if req['status'] == 'ok':
             if req['data']['movie_count'] > 0:
                 arr, count = [], 1
                 for r in req['data']['movies']:
-                    arr.append({'id': count, 'title': r['title'], 'magnet': r['torrents'][0]['url']})
+                    title = '%s (%s) (%s)' % (r['title'], r['year'], r['torrents'][0]['quality'])
+                    arr.append({'id': count, 'title': title, 'magnet': r['torrents'][0]['url']})
                     count += 1
                 return arr
 
