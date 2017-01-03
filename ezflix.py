865edb9199e82fb800a6bfdbb1f05f1c86e028a2
Anthony Bloomer <ant0@protonmail.ch>
2017-01-03 01:16:47 +0000

    Add YTS support

--- ezflix.py
+++ ezflix.py
@@ -1,32 +1,63 @@
-from bs4 import BeautifulSoup
-import requests
 import subprocess
 import argparse
+from bs4 import BeautifulSoup
+import requests
 import sys
+from urllib import quote_plus
 
 parser = argparse.ArgumentParser()
+parser.add_argument('media_type', nargs='?', default='tv')
 parser.add_argument('query')
 parser.add_argument('latest', nargs='?', default='0')
-
 args = parser.parse_args()
 
-query = args.query.replace(' ', '-').lower()
-url = 'https://eztv.ag/search/' + query
 
-req = requests.get(url)
-soup = BeautifulSoup(req.text, 'html.parser')
-
-if __name__ == '__main__':
+def show(q):
+    url = 'https://eztv.ag/search/' + q
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'html.parser')
     magnets = soup.find_all('a', {'class': 'magnet'}, href=True)
 
     if magnets is None:
         sys.exit('No results found')
-    
-    results = []
-    id = 1
+
+    arr = []
+    c = 1
     for magnet in magnets:
-        results.append({'id': id, 'title': magnet['title'][:-12], 'magnet': magnet['href']})
-        id += 1
+        arr.append({'id': c, 'title': magnet['title'][:-12], 'magnet': magnet['href']})
+        c += 1
+
+    return arr
+
+
+def movie(q):
+    request = requests.get('https://yts.ag/api/v2/list_movies.json?query_term=%s' % q)
+    if request.status_code == 200:
+        arr = request.json()
+        if arr['status'] == 'ok':
+            if arr['data']['movie_count'] > 0:
+                l = []
+                c = 1
+                for r in arr['data']['movies']:
+                    l.append({'id': c, 'title': r['title'], 'magnet': r['torrents'][0]['url']})
+                    c += 1
+                return l
+
+
+if __name__ == '__main__':
+
+    query = args.query
+
+    results = []
+
+    if args.media_type == 'tv':
+        results = show(query.replace(' ', '-').lower())
+
+    elif args.media_type == 'movie':
+        results = movie(quote_plus(query))
+
+    else:
+        sys.exit('Incorrect media type.')
 
     if args.latest == "latest":
         latest = results[0]
@@ -34,6 +65,7 @@ if __name__ == '__main__':
         subprocess.Popen(['/bin/bash', '-c', 'peerflix "%s" --mpv' % latest['magnet']])
 
     else:
+
         for result in results:
             print '| %s | %s' % (result['id'], result['title'])
 
