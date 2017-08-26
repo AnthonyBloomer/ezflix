4da67f01649bb88ef6d1b1b4182d07e5ba28f64e
Anthony Bloomer <ant0@protonmail.ch>
2017-08-26 12:33:23 +0100

    Update

--- ezflix/extractor/yts.py
+++ ezflix/extractor/yts.py
@@ -12,4 +12,7 @@ def yts(q, limit=20):
                     title = '%s (%s) (%s)' % (r['title'], r['year'], r['torrents'][0]['quality'])
                     arr.append({'id': count, 'title': title, 'magnet': r['torrents'][0]['url']})
                     count += 1
+
+                    if count == limit + 1:
+                        break
                 return arr
