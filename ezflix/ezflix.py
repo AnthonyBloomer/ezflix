eb14fbedeada5e9bfd8f4a591bc8a178398945c2
Anthony Bloomer <ant0@protonmail.ch>
2017-09-03 12:18:48 +0100

    Update

--- ezflix/ezflix.py
+++ ezflix/ezflix.py
@@ -13,6 +13,8 @@ except:
 parser = Parser()
 args = parser.parse()
 
+media_player = args.media_player
+
 if not cmd_exists('peerflix'):
     sys.exit('This program requires Peerflix. https://github.com/mafintosh/peerflix')
 
@@ -23,103 +25,74 @@ if not args.query:
     sys.exit(colorful.red("Search query not valid."))
 
 
-class Ezflix(object):
-    def __init__(self,
-                 media_type,
-                 search_query,
-                 quality,
-                 sort_by,
-                 minimum_rating,
-                 sort_order,
-                 latest=False,
-                 player='mpv',
-                 limit=20,
-                 subtitles=False):
+def get_magnet(val, torrents):
+    for result in torrents:
+        if result['id'] == int(val):
+            return result
+    return
 
-        self.media_type = media_type
-        self.search_query = search_query
-        self.latest = latest
-        self.player = player
-        self.torrents = []
-        self.limit = limit
-        self.subtitles = subtitles
-        self.sort_by = sort_by
-        self.sort_order = sort_order
-        self.quality = quality
-        self.minimum_rating = minimum_rating
 
-    def get_magnet(self, val):
-        for result in self.torrents:
-            if result['id'] == int(val):
-                return result
-        return
+def get_torrents():
+    torrents = []
 
-    def get_torrents(self):
-        if self.media_type == 'tv':
-            self.torrents = eztv(self.search_query.replace(' ', '-').lower(), limit=self.limit)
+    if args.media_type == 'tv':
+        torrents = eztv(args.query.replace(' ', '-').lower(), limit=args.limit)
 
-        elif self.media_type == 'movie':
-            self.torrents = yts(q=quote_plus(self.search_query),
-                                limit=self.limit,
-                                sort_by=self.sort_by,
-                                sort_order=self.sort_order,
-                                quality=self.quality,
-                                minimum_rating=self.minimum_rating
-                                )
+    elif args.media_type == 'movie':
+        torrents = yts(q=quote_plus(args.query),
+                       limit=args.limit,
+                       sort_by=args.sort_by,
+                       sort_order=args.sort_order,
+                       quality=args.quality,
+                       minimum_rating=args.minimum_rating
+                       )
 
-    def display(self):
-        if self.torrents is None or not len(self.torrents) > 0:
-            sys.exit(colorful.red('No results found.'))
+    return torrents
 
-        if self.latest:
-            latest = self.torrents[0]
-            print("Playing " + latest['title'])
-            peerflix(latest['magnet'], self.player, self.media_type, self.subtitles)
+
+def display(torrents):
+    if torrents is None or len(torrents) == 0:
+        sys.exit(colorful.red('No results found.'))
+
+    if args.latest:
+        latest = torrents[0]
+        print("Playing " + latest['title'])
+        peerflix(latest['magnet'], media_player, args.media_type, args.subtitles)
+        sys.exit()
+
+    for result in torrents:
+        print(colorful.bold('| ' + str(result['id'])) + ' | ' + result['title'])
+
+
+def select(torrents):
+    print("Make selection: ")
+    while True:
+        read = raw_input()
+
+        if read == 'quit':
             sys.exit()
 
-        for result in self.torrents:
-            print(colorful.bold('| ' + str(result['id'])) + ' | ' + result['title'])
+        try:
+            int_val = int(read)
+        except ValueError:
+            print(colorful.red('Invalid selection.'))
+            continue
 
-    def select(self):
-        print("Make selection: ")
-        while True:
-            read = raw_input()
+        magnet = get_magnet(int_val, torrents=torrents)
 
-            if read == 'quit':
-                sys.exit()
+        if not magnet:
+            print(colorful.red('Invalid selection.'))
+            continue
 
-            try:
-                int_val = int(read)
-            except:
-                print(colorful.red('Invalid selection.'))
-                continue
+        print("Playing " + magnet['title'])
 
-            magnet = self.get_magnet(int_val)
-
-            if not magnet:
-                print(colorful.red('Invalid selection.'))
-                continue
-
-            print("Playing " + magnet['title'])
-
-            peerflix(magnet['magnet'], self.player, self.media_type, self.subtitles)
+        peerflix(magnet['magnet'], media_player, args.media_type, args.subtitles)
 
 
 def main():
-    ezflix = Ezflix(media_type=args.media_type,
-                    search_query=args.query,
-                    latest=args.latest,
-                    player=args.media_player,
-                    limit=int(args.limit),
-                    subtitles=args.subtitles,
-                    sort_by=args.sort_by,
-                    sort_order=args.sort_order,
-                    quality=args.quality,
-                    minimum_rating=args.minimum_rating
-                    )
-    ezflix.get_torrents()
-    ezflix.display()
-    ezflix.select()
+    torrents = get_torrents()
+    display(torrents)
+    select(torrents)
 
 
 if __name__ == '__main__':
