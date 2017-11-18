80978daba1f4aea7dafbfa0b5c2f4e2b547ec84e
Anthony Bloomer <ant0@protonmail.ch>
2017-11-18 12:56:45 +0000

    Rewrote ytf function to make more readable.

--- ezflix/extractors/yts.py
+++ ezflix/extractors/yts.py
@@ -13,16 +13,20 @@ def yts(q, limit=20, minimum_rating=4, quality='720p', sort_by='seeds', sort_ord
     }
     req = requests.get('https://yts.ag/api/v2/list_movies.json', params=params)
 
-    if req.status_code == 200:
-        req = req.json()
-        if req['status'] == 'ok':
-            if req['data']['movie_count'] > 0:
-                arr, count = [], 1
-                for r in req['data']['movies']:
-                    for torrent in r['torrents']:
-                        title = '%s (%s) (%s)' % (r['title'], r['year'], torrent['quality'])
-                        arr.append({'id': count,
-                                    'title': title,
-                                    'magnet': torrent['url']})
-                        count += 1
-                return arr
+    if not req.ok:
+        return
+
+    req = req.json()
+
+    if not req['status'] == 'ok' or not req['data']['movie_count'] > 0:
+        return
+
+    arr, count = [], 1
+    for r in req['data']['movies']:
+        for torrent in r['torrents']:
+            title = '%s (%s) (%s)' % (r['title'], r['year'], torrent['quality'])
+            arr.append({'id': count,
+                        'title': title,
+                        'magnet': torrent['url']})
+            count += 1
+    return arr
