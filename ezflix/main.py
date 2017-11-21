0ff56ce539786ac7884f6aa713ca46650a958028
Anthony Bloomer <ant0@protonmail.ch>
2017-11-21 02:03:29 +0000

    Update main.py

--- ezflix/main.py
+++ ezflix/main.py
@@ -32,65 +32,44 @@ def main():
                     )
 
     torrents = ezflix.get_torrents()
-
     if torrents is None or len(torrents) == 0:
         sys.exit(colorful.red('No results found.'))
-
     if args.latest:
-
         latest = torrents[0]
         file_path = ''
-
         if args.subtitles:
             file_path = ezflix.find_subtitles(latest['title'])
-
         print("Playing " + latest['title'])
-
         peerflix(latest['magnet'], media_player, args.media_type, args.subtitles, args.remove, file_path)
-
         sys.exit()
-
     row = PrettyTable()
     row.field_names = ["Id", "Torrent"]
     row.align = 'l'
     for result in torrents:
         row.add_row([result['id'], result['title']])
-
     print(row)
-
     print(colorful.bold("Make selection: (Enter quit to close the program)"))
-
     try:
         input = raw_input
     except NameError:
         pass
-
     while True:
-
         read = input()
-
         if read == 'quit':
             sys.exit()
-
         try:
             int_val = int(read)
         except (ValueError, TypeError) as error:
             print(colorful.red('Invalid selection.'))
             continue
-
         magnet = ezflix.get_magnet(int_val)
-
         if not magnet:
             print(colorful.red('Invalid selection.'))
             continue
-
         print("Playing " + magnet['title'])
-
         file_path = ''
-
         if args.subtitles:
             file_path = ezflix.find_subtitles(magnet['title'])
-
         peerflix(magnet['magnet'], media_player, args.media_type, args.subtitles, args.remove, file_path)
 
 
