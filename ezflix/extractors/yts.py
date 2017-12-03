1e84152da53e3a2117a0610d54428c39ed707791
Anthony Bloomer <ant0@protonmail.ch>
2017-12-03 09:35:11 +0000

    Update yts.py

--- ezflix/extractors/yts.py
+++ ezflix/extractors/yts.py
@@ -1,9 +1,9 @@
 import requests
 
 
-def yts(q, quality=None, limit=20, minimum_rating=4, sort_by='seeds', sort_order='desc'):
+def yts(query_term, quality=None, limit=20, minimum_rating=4, sort_by='seeds', sort_order='desc'):
     params = {
-        'query_term': q,
+        'query_term': query_term,
         'sort_by': sort_by,
         'sort_order': sort_order,
         'limit': limit,
@@ -24,14 +24,12 @@ def yts(q, quality=None, limit=20, minimum_rating=4, sort_by='seeds', sort_order
     arr, count = [], 1
     for r in req['data']['movies']:
         for torrent in r['torrents']:
-
+            title = '%s (%s) (%s)' % (r['title'], r['year'], torrent['quality'])
             if quality is not None:
                 if quality == torrent['quality']:
-                    title = '%s (%s) (%s)' % (r['title'], r['year'], torrent['quality'])
                     arr.append({'id': count, 'title': title, 'magnet': torrent['url']})
                     count += 1
             else:
-                title = '%s (%s) (%s)' % (r['title'], r['year'], torrent['quality'])
                 arr.append({'id': count, 'title': title, 'magnet': torrent['url']})
                 count += 1
     return arr
