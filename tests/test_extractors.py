1ec1b40810ebeecfcba804baa294db7e829bff71
Anthony Bloomer <ant0@protonmail.ch>
2017-12-03 09:31:05 +0000

    Update test_extractors.py

--- tests/test_extractors.py
+++ tests/test_extractors.py
@@ -40,3 +40,15 @@ class ExtractorTests(unittest.TestCase):
         """
         torrents = eztv(q='Breaking Bad', limit=1)
         self.assertTrue(len(torrents) == 1)
+        
+    def test_yts_quality(self):
+        torrents = yts(query_term='Scarface', limit=1, quality='720p')
+        self.assertTrue(len(torrents) > 0)
+        first = torrents[0]
+        self.assertTrue('720p' in first['title'])
+
+    def test_eztv_quality(self):
+        torrents = eztv(q='Breaking Bad', limit=20, quality='720p')
+        self.assertTrue(len(torrents) > 0)
+        self.assertTrue('Breaking Bad' in torrents[0]['title'])
+        self.assertTrue('720p' in torrents[0]['title'])
