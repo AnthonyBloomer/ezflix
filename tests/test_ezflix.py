0bcb2f9c026d2951b75818130b4bfbb694cb1766
Anthony Bloomer <ant0@protonmail.ch>
2017-12-07 15:26:57 +0000

    Update test_ezflix.py

--- tests/test_ezflix.py
+++ tests/test_ezflix.py
@@ -48,5 +48,5 @@ class EzflixTests(unittest.TestCase):
         cur_dir = os.getcwd()
         file_list = os.listdir(cur_dir)
         for f in file_list:
-            if movie_tile in f:
+            if movie_title in f:
                 os.remove(f)
