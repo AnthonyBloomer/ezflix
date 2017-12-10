fcea0ca09562ef9fde4ea3206646b7fea341a63f
Anthony Bloomer <ant0@protonmail.ch>
2017-12-10 17:39:50 +0000

    New version adds support to diff versions of Python

--- ezflix/__version__.py
+++ ezflix/__version__.py
@@ -1,2 +1,2 @@
-VERSION_TUPLE = (1, 4, 3)
+VERSION_TUPLE = (1, 4, 4)
 __version__ = ".".join(map(str, VERSION_TUPLE))
--- setup.py
+++ setup.py
@@ -80,7 +80,6 @@ setup(
         'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
