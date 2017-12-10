e41a4ad3b198dfcab07ff8b19fb15d9ffed6cc33
Anthony Bloomer <ant0@protonmail.ch>
2017-12-10 17:30:12 +0000

    Testing build across multiple versions of Python

--- setup.py
+++ setup.py
@@ -79,8 +79,14 @@ setup(
         'Intended Audience :: End Users/Desktop',
         'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     cmdclass={
         'publish': PublishCommand,
